7d486f9c99cf3863ce4b583d0e1315da33b77ab0
martynvalenz <martynvalenz@gmail.com>
2024-08-03 11:22:29 -0600

    chore: Add new chat dialog to ChatSidebar and implement useDebounce hook

--- src/app/(main)/messages/ChatSidebar.tsx
+++ src/app/(main)/messages/ChatSidebar.tsx
@@ -3,7 +3,8 @@ import { useSession } from "../SessionProvider"
 import { Button } from "@/components/ui/button"
 import { MailPlus, Menu, X } from "lucide-react"
 import { cn } from "@/lib/utils"
-import { useCallback } from "react"
+import { useCallback, useState } from "react"
+import NewChatDialog from "./NewChatDialog"
 
 interface ChatSidebarProps {
   open:boolean
@@ -65,17 +66,34 @@ interface MenuHeaderProps {
 }
 
 const MenuHeader = ({onClose}:MenuHeaderProps) => {
+  const [showNewChatDialog, setShowNewChatDialog] = useState(false)
+  
   return (
-    <div className="flex items-center gap-3 p-2">
-      <div className="h-full md:hidden">
-        <Button size="icon" variant="ghost" onClick={onClose}>
-          <X className="size-5"/>
+    <>
+      <div className="flex items-center gap-3 p-2">
+        <div className="h-full md:hidden">
+          <Button size="icon" variant="ghost" onClick={onClose}>
+            <X className="size-5"/>
+          </Button>
+        </div>
+        <h1 className="me-auto text-xl font-bold md:ms-2">Messages</h1>
+        <Button size="icon" variant="ghost" title="Start new chat"
+          onClick={()=>setShowNewChatDialog(true)}
+        >
+          <MailPlus className="size-5"/>
         </Button>
       </div>
-      <h1 className="me-auto text-xl font-bold md:ms-2">Messages</h1>
-      <Button size="icon" variant="ghost" title="Start new chat">
-        <MailPlus className="size-5"/>
-      </Button>
-    </div>
+      {
+        showNewChatDialog && (
+          <NewChatDialog
+            onOpenChange={setShowNewChatDialog}
+            onChatCreated={() => {
+              setShowNewChatDialog(false)
+              onClose()
+            }}
+          />
+        )
+      }
+    </>
   )
 }
--- src/app/(main)/messages/NewChatDialog.tsx
+++ src/app/(main)/messages/NewChatDialog.tsx
@@ -1,5 +1,8 @@
-import { Dialog } from "@/components/ui/dialog"
-import { DialogContent } from "@radix-ui/react-dialog"
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog"
+import { useToast } from "@/components/ui/use-toast"
+import { useChatContext } from "stream-chat-react"
+import { useSession } from "../SessionProvider"
+import { useState } from "react"
 
 interface Props {
   onOpenChange:(open:boolean)=>void
@@ -10,9 +13,22 @@ const NewChatDialog = ({
   onOpenChange,
   onChatCreated
 }:Props) => {
+  const {
+    client,
+    setActiveChannel
+  } = useChatContext()
+  const {toast} = useToast()
+  const{user:loggedInUser} = useSession()
+  const [searchInput, setSearchInput] = useState('')
+  
+
   return (
     <Dialog open onOpenChange={onOpenChange}>
-      <DialogContent></DialogContent>
+      <DialogContent className="bg-card p-0">
+        <DialogHeader>
+          <DialogTitle>New Chat</DialogTitle>
+        </DialogHeader>
+      </DialogContent>
     </Dialog>
   )
 }
